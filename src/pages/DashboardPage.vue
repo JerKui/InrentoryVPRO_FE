c97c7f99bbb00d77c317ce1384a473090dcec152
jerku <jerkui99@gmail.com>
2022-06-23 16:45:03 +0200

    Testing stuff

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,7 +1,19 @@
 <template>
 <default-layout>
 <div class="container">
-    <h1>Welcome to the dashboard!</h1>
+    <h3>hello</h3>
+    <table class="tableDesign">
+        <thead>
+            <tr>
+                <th v-for="(column, index) in columns" v-bind:key="index" class="tableHead">{{column}}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(row, index) in rows" v-bind:key="index">
+                <td v-for="(rowItem, itemIndex) in row" v-bind:key="itemIndex" class="tableItem">{{rowItem}}</td>   
+            </tr>
+        </tbody>
+    </table>
 </div>
 </default-layout>
 </template>
@@ -10,11 +22,77 @@
 import DefaultLayout from '@/layouts/DefaultLayout'
 </script>
 
+<script>
+// import axios from '../axios-common'
+
+export default {
+    name: 'TableData',
+    data() { 
+        return {
+            rows: [
+                [
+                    "Manoj", "25", "Software Developer", "1997"
+                ],
+                [
+                    "John", "25", "Lawyer", "1997"
+                ],
+                [
+                    "Lily", "23", "Saleswoman", "1999"
+                ],
+                [
+                    "Manoj", "25", "Software Developer", "1997"
+                ],
+                [
+                    "John", "25", "Lawyer", "1997"
+                ],
+                [
+                    "Lily", "23", "Saleswoman", "1999"
+                ],
+                [
+                    "Manoj", "25", "Software Developer", "1997"
+                ],
+                [
+                    "John", "25", "Lawyer", "1997"
+                ],
+                [
+                    "Lily", "23", "Saleswoman", "1999"
+                ],
+                [
+                    "Manoj", "25", "Software Developer", "1997"
+                ],
+                [
+                    "John", "25", "Lawyer", "1997"
+                ],
+                [
+                    "Lily", "23", "Saleswoman", "1999"
+                ]
+            ],
+            columns: [
+                'Name',
+                'Age',
+                'Job',
+                'Year of Birth'
+            ]
+        }
+    }
+}
+</script>
+
 <style>
 .container {
     padding: 0;
     margin: 0;
     width: 100vw;
     background-color: #fff;
+}
+
+.tableDesign {
+    border-collapse: collapse;
+}
+
+.tableHead, .tableItem {
+    width: 120px;
+    padding: 8px;
+    border: 1px solid;
 }
 </style>
--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,20 +1,117 @@
 <template>
 <default-layout>
-<div class="container">
-    <h1>Welcome to the product!</h1>
+<div>
+    <a :href="url">Best website ever</a>
+
+    <button @click="hidePanel">
+        <span v-if="showBooks">Hide Panel</span>
+        <span v-else>Show Panel</span>
+    </button>
+
+    <div v-if="showBooks">
+    <ul>
+        <li v-for="book in filteredBooks" :key="book.title" :class="{ fav: book.isFav}" @click="toggleFav(book)">
+        <img :src="book.img" :alt="book.title">
+        <h3>{{ book.title }}</h3>
+        <p>{{ book.author }}</p>
+        </li>
+    </ul>
+    </div>
+
+    <br>
+    <!-- Mouse Events -->
+    <!-- <div class="box" @mouseover="handleEvent($event, 5)">mouseover</div>
+    <div class="box" @mouseleave="handleEvent($event, 5)">mouseleave</div>
+    <div class="box" @dblclick="handleEvent($event, 5)">double click</div>
+    <div class="box">test</div> -->
+
+
 </div>
 </default-layout>
 </template>
+
+<style>
+
+.box {
+    padding: 100px 0;
+    width: 400px;
+    text-align: center;
+    background-color: #ddd;
+    margin: 20px;
+    display: inline-block;
+}
+
+body{
+    background-color: #eee;
+    max-width: 960px;
+    margin: 20px auto;
+}
+
+p, h3, ul {
+    margin: 0;
+    padding: 0;
+}
+
+ul {
+    padding: 24px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    justify-content: center;
+}
+
+li {
+    list-style-type: none;
+    background: #fff;
+    margin: 20px auto;
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 12px;
+}
+
+li.fav{
+    background-color: #9999;
+}
+</style>
 
 <script setup>
 import DefaultLayout from '@/layouts/DefaultLayout'
 </script>
 
-<style>
-.container {
-    padding: 0;
-    margin: 0;
-    width: 100vw;
-    background-color: #fff;
+<script>
+export default {
+    name: 'ProductPage',
+    data() {
+        return {
+            url: 'http://google.nl',
+            showBooks: true,
+            books: [
+                { title: 'Red Dead Redemption 2', author: 'Rockstar', img: 'https://cdn.akamai.steamstatic.com/steam/apps/1174180/capsule_sm_120.jpg?t=1618851907', isFav: true},
+                { title: 'Fallout New Vegas', author: 'Bethesda', img: 'https://cdn.akamai.steamstatic.com/steam/apps/22380/capsule_sm_120.jpg?t=1586905021', isFav: true},
+                { title: 'Tyranny', author: 'Steam', img: 'https://cdn.akamai.steamstatic.com/steam/apps/362960/capsule_sm_120.jpg?t=1615985862', isFav: true}
+            ]
+        }
+    },
+    methods: {
+        hidePanel() {
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav;
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.title == 'Fallout New Vegas');
+        }
+    }
 }
-</style>
+</script>
