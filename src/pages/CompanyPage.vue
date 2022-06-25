ec4f47cbd3727a09bf8d2e30cc136e4d28a5949c
jerku <jerkui99@gmail.com>
2022-06-25 20:18:55 +0200

    Many other styling changes

--- src/pages/CompanyPage.vue
+++ src/pages/CompanyPage.vue
@@ -2,93 +2,17 @@
 <div v-if="showAddCategory"><AddCategory @close="toggleAddCategory"/></div>
 <div v-if="showAddProduct"><AddProduct @close="toggleAddProduct"/></div>
 <default-layout>
-
-    <div class="containerLeft">
-        <div class="leftHeader">
-            <div class="leftItemInfo">
-                <p># of items stored</p>
-                <h1> {{ productCount }}</h1>
-            </div>
-            <div class="leftItemInfo">
-                <p># of categories stored</p>
-                <h1> {{ categoryCount }}</h1>
-            </div>
-            <hr>
-            <input type="text" placeholder="Search here">
-            <hr>
-            <div class="form">
-                <table>
-                    <thead>
-                        <tr>
-                            <th v-for="productColumn in productColumns" :key="productColumn.id"> {{ productColumn }}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="products in productData" :key="products.id">
-                            <td> #{{ products.id }} </td>
-                            <td> {{ products.name }} </td>
-                            <td> {{ products.description }} </td>
-                            <td> {{ products.image }} </td>
-                            <td> {{ products.stock }} </td>
-                            <td> €{{ products.price }} </td>
-                            <td> {{ products.productline.description }} </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+<div class="container">
+    <button @click="toggleAddCategory" class="buttonCategory">
+            <p>Add category</p>
+        </button>
+    <p @click="toggleAddProduct" class="buttonProduct">Add item</p>
+    <div class="product">
+        <div class="divProduct">
+            div
         </div>
     </div>
-
-    <div class="containerRight">
-        <div class="rightContent">
-            <div class="rightButtons">
-                <button @click="toggleAddCategory" class="buttonCategory">
-                    <h4>Set up category</h4>
-                    <p>Create new category</p>
-                </button>
-                <button @click="toggleAddProduct" class="buttonProduct">
-                    <h4>Add new product</h4>
-                    <p>Store an item</p>
-                </button>
-            </div>
-            <hr>
-            <!-- <div class="rightCategories">
-                <h1>Top categories</h1>
-                <div class="categories">
-                    <div class="categoriesItem">
-                        <font-awesome-icon icon="fa fa-sign-out"/>
-                        <div class="categoriesItem_middle">
-                            <h4>Memory Card</h4>
-                            <p>115 items</p>
-                        </div>
-                    </div>
-                    <div class="categoriesItem">
-                        <font-awesome-icon icon="fa fa-sign-out"/>
-                        <div class="categoriesItem_middle">
-                            <h4>Memory Card</h4>
-                            <p>115 items</p>
-                        </div>
-                    </div>
-                    <div class="categoriesItem">
-                        <font-awesome-icon icon="fa fa-sign-out"/>
-                        <div class="categoriesItem_middle">
-                            <h4>Memory Card</h4>
-                            <p>115 items</p>
-                        </div>
-                    </div>
-                    <div class="categoriesItem">
-                        <font-awesome-icon icon="fa fa-sign-out"/>
-                        <div class="categoriesItem_middle">
-                            <h4>Memory Card</h4>
-                            <p>115 items</p>
-                        </div>
-                    </div> 
-                </div>                               
-            </div> -->
-            <hr>
-        </div>
-    </div>
-
+</div>
 </default-layout>
 </template>
 
@@ -172,96 +96,4 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 32px;
-}
-/* Styling left side */
-.containerLeft {
-    width: 70%;
-    background-color: white;
-}
-
-.leftHeader {
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    padding: 48px;
-}
-.leftItemInfo h1 {
-    font-size: 64px;
-}
-
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-  width: 100%;
-  table-layout: fixed ;
-}
-
-th {
-    background-color: black;
-    color: white;
-}
-
-th, td {
-    padding: 16px;
-}
-
-/* Styling right side */
-.containerRight {
-    border-style: solid;
-    border-right: 0;
-    border-top: 0;
-    border-bottom: 0;
-    border-width: 4px;
-    border-color: #F3F3F4;
-    display: flex;
-    flex-direction: column;
-    width: 30%;
-}
-
-.rightContent { 
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-    padding: 48px;
-}
-
-.rightButtons {
-    display: flex;
-    flex-direction: column;
-    gap: 12px;
-}
-
-.rightButtons button {
-    height: 80px;
-}
-
-.categories {
-    padding-top: 24px;
-    padding-bottom: 24px;
-    display: flex;
-    flex-direction: column;
-    gap: 24px;
-}
-
-.categoriesItem p {
-    font-weight: 300;
-    font-size: 12px;
-    color: #919191; 
-}
-
-.categoriesItem {
-    display: flex;
-    gap: 24px;
-    justify-content: left;
-    align-items: center;
-}
-
-
-/* Buttons Components */
 </style>
