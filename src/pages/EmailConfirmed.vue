00532faf6d4ca552ddab4585510b55ea0ac2c173
jerku <jerkui99@gmail.com>
2022-07-05 09:54:09 +0200

    Commit

---
+++ src/pages/EmailConfirmed.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="confirm">
+        <router-link to="/">click hier</router-link>
+    </div>
+</template>
+
+<script setup>
+import axios from '../axios-common'
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute();
+
+onMounted(() => {
+  const tokenObj = route.query
+  axios.get('/registration/confirm', {
+    params: {
+      token: tokenObj.token}
+    })
+  .then((res) => {
+    console.log(res)
+  })
+  .catch((error) => console.log(error))
+})
+
+</script>
+
+<style>
+
+.confirm {
+    height: 100vh;
+    width: 100vw;
+}
+
+</style>
